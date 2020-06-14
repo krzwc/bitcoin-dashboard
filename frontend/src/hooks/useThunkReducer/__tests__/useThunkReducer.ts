89cf352013796e71701adda5420b60230178a485
Kacper Krzywiec <krzywiec@gmail.com>
2020-06-14 09:53:33 +0200

    Add useThunkReducer hook unit tests

---
+++ frontend/src/hooks/useThunkReducer/__tests__/useThunkReducer.ts
@@ -0,0 +1,40 @@
+import useThunkReducer from 'hooks/useThunkReducer';
+import { Action, State } from '../../interfaces';
+import ACTIONS from 'hooks/actions';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+const mockedReducer = (state: State, action: Action): State => {
+    switch (action.type) {
+        case ACTIONS.FETCHING:
+            return mockedChangedState;
+        default:
+            return state;
+    }
+};
+
+const mockedInitialState: State = {
+    result: '',
+    loading: false,
+    error: '',
+};
+
+const mockedChangedState = {
+    result: 'test',
+    loading: false,
+    error: '',
+};
+
+describe('useThunkReducer', () => {
+    test('should return initial state as default', () => {
+        const { result } = renderHook(() => useThunkReducer(mockedReducer, mockedInitialState));
+        expect(result.current[0]).toStrictEqual(mockedInitialState);
+    });
+    test('dispatched action changes state', () => {
+        const { result } = renderHook(() => useThunkReducer(mockedReducer, mockedInitialState));
+        const dispatch = result.current[1];
+        act(() => {
+            dispatch({ type: ACTIONS.FETCHING });
+        });
+        expect(result.current[0]).toStrictEqual(mockedChangedState);
+    });
+});
